9d90c29e0f571fc23dba59834ae3c26cf38038ae
clarklayos123@gmail.com <clarklayos123@gmail.com>
2025-04-11 04:00:01 +0800

    header fixed

--- js/main.js
+++ js/main.js
@@ -40,6 +40,7 @@ let changes_char = [
 // }, 300)
 
 let nav_bar = document.querySelector('.responsive-bar')
+let header_bar = document.querySelector('#header-page')
 let side_bar = document.querySelector('.side-bar')
 let btn_side = document.querySelector('.btn-sidebar')
 
@@ -47,7 +48,9 @@ nav_bar.onclick = () => {
     // side_bar.style.transform = 'translateX(0%)'
     // side_bar.style.transition = '0.3s ease'
     side_bar.style.display = 'flex'
+    header_bar.style.display = "none"
 }
 btn_side.onclick = () =>{
     side_bar.style.display = 'none'
+    header_bar.style.display = "flex"
 }
